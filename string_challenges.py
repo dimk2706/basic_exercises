a2cb3d6108edab8e67a3068f692dda6b414443b8
Dmitriy Korlykhanov <dimakorla@gmail.ru>
2023-09-12 22:16:43 +0300

    Minor fixes

--- string_challenges.py
+++ string_challenges.py
@@ -1,17 +1,17 @@
-def main():
+def exercise1():
     # Вывести последнюю букву в слове
     print("Вывести последнюю букву в слове")
     word = 'Архангельск'
     print(f"{word[-1]}")
 
-
+def exercise2():
     # Вывести количество букв "а" в слове
     print("Вывести количество букв а в слове")
     word = 'Архангельск'
     number =word.count("А") + word.count("а")
     print(number)
 
-
+def exercise3():
     # Вывести количество гласных букв в слове
     print("Вывести количество гласных букв в слове")
     word = 'Архангельск'
@@ -22,14 +22,14 @@ def main():
             number = number + 1
     print(number)
 
-
+def exercise4():
     # Вывести количество слов в предложении
     print("Вывести количество слов в предложении")
     sentence = 'Мы приехали в гости'
     sentence_list =sentence.split()
     print(len(sentence_list))
 
-
+def exercise5():
     # Вывести первую букву каждого слова на отдельной строке
     print("Вывести первую букву каждого слова на отдельной строке")
     sentence = 'Мы приехали в гости'
@@ -37,7 +37,7 @@ def main():
     for word in sentence_list:
         print(word[0])        
 
-
+def exercise6():
     # Вывести усреднённую длину слова в предложении
     print("Вывести усреднённую длину слова в предложении")
     sentence = 'Мы приехали в гости'
@@ -48,4 +48,9 @@ def main():
     print(words_len/len(sentence_list))
 
 if __name__ == "__main__":
-    main()
+    exercise1()
+    exercise2()
+    exercise3()
+    exercise4()
+    exercise5()
+    exercise6()
